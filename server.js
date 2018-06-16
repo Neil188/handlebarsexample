8aeaf84becaf061bad066e94c8e5e3420290c579
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-06-16 17:25:59 +0100

    express serve static file

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// app.use('/public', express.static('public)'));
+app.use( express.static('public') );
+
 app.get('/', function (req,res) {
     res.send('hello world');
 });
